af9b93a2bc60eeab3e1bc758ee4892bc9b3b1152
rohit maharjan <rmaharjan61@gmail.com>
2020-11-20 15:41:29 +0545

    working all

---
+++ assignment_1/assignment_1/assignment_1/UserData.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UserData.aspx.cs" Inherits="assignment_1.UserData" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="background-color:black; color: white; text-align: center">
+            <br />
+            <h1>Enter User Data</h1>
+            <br />
+        </div>
+        <br /> <br />
+        <div style="height: 533px; text-align: center" >
+            <asp:Panel ID="Panel1" runat="server" Height="528px">
+                <asp:Label ID="Label1" runat="server" Text="First Name :"></asp:Label>
+                <asp:TextBox ID="txtFirstName" runat="server" style="margin-left: 25px" Width="250px"></asp:TextBox>
+                <asp:Label ID="errFirstName" runat="server" ForeColor="#CC0000" Text="*Enter FirstName" Visible="False"></asp:Label>
+                <br />
+                <br />
+                <asp:Label ID="Label2" runat="server" Text="Last Name :"></asp:Label>
+                <asp:TextBox ID="txtLastName" runat="server" style="margin-left: 29px" Width="248px"></asp:TextBox>
+                <asp:Label ID="errLastName" runat="server" ForeColor="#CC0000" Text="*Enter LastName" Visible="False"></asp:Label>
+                <br />
+                <br />
+                <asp:Label ID="Label3" runat="server" Text="Email :"></asp:Label>
+                <asp:TextBox ID="txtEmail" runat="server" style="margin-left: 60px" Width="248px"></asp:TextBox>
+                <asp:Label ID="errEmail" runat="server" ForeColor="#CC0000" Text="*Enter Email" Visible="False"></asp:Label>
+                <br />
+                <br />
+                <asp:Label ID="Label4" runat="server" Text="Mobile No. :"></asp:Label>
+                <asp:TextBox ID="txtMobile" runat="server" style="margin-left: 23px" Width="249px"></asp:TextBox>
+                <asp:Label ID="errMobile" runat="server" ForeColor="#CC0000" Text="*Enter Mobile No." Visible="False"></asp:Label>
+                <br />
+                <br />
+                <asp:Label ID="Label5" runat="server" Text="Address :"></asp:Label>
+                <asp:TextBox ID="txtAddress" runat="server" style="margin-left: 42px" Width="251px"></asp:TextBox>
+                <asp:Label ID="errAddress" runat="server" ForeColor="#CC0000" Text="*Enter Address" Visible="False"></asp:Label>
+                <br />
+                <br />
+                <asp:Label ID="Label6" runat="server" Text="Gender :"></asp:Label>
+                 &nbsp &nbsp &nbsp &nbsp &nbsp<asp:RadioButton ID="radMale" runat="server" Text="Male" GroupName="radGender" />
+                 &nbsp &nbsp &nbsp &nbsp <asp:RadioButton ID="radFemale" runat="server" Text="Female" GroupName="radGender" />
+                <asp:Label ID="errGender" runat="server" ForeColor="#CC0000" Text="*Select One" Visible="False"></asp:Label>
+                <br />
+                <br />
+                <asp:Button ID="btnSave" runat="server" style="margin-left: 78px" Text="Save" Width="72px" OnClick="btnSave_Click" />
+                <asp:Button ID="btnReset" runat="server" style="margin-left: 24px" Text="Reset" Width="74px" OnClick="btnReset_Click" />
+                <br />
+                <br />
+            </asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ assignment_1/assignment_1/assignment_1/UserData.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace assignment_1
+{
+    public partial class UserData : System.Web.UI.Page
+    {
+        string gender;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            txtFirstName.Focus();
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            string FirstName = txtFirstName.Text.Trim();
+            string LastName = txtLastName.Text.Trim();
+            string Email= txtEmail.Text.Trim();
+            string Address = txtAddress.Text.Trim();
+            string Mobile = txtMobile.Text.Trim();
+            
+
+
+            // Validation 
+            if (FirstName == "")
+            {
+                errFirstName.Visible = true;
+                txtFirstName.Focus();
+            }
+            else
+            {
+                errFirstName.Visible = false;
+            }
+
+            if (LastName == "")
+            {
+                errLastName.Visible = true;
+                txtLastName.Focus();
+            }
+            else
+            {
+                errLastName.Visible = false;
+            }
+
+            if (Email == "")
+            {
+                errEmail.Visible = true;
+                txtEmail.Focus();
+            }
+            else
+            {
+                errEmail.Visible = false;
+            }
+
+            if (Address == "")
+            {
+                errAddress.Visible = true;
+                txtAddress.Focus();
+            }
+            else
+            {
+                errAddress.Visible = false;
+            }
+            if (Mobile == "")
+            {
+                errMobile.Visible = true;
+                txtMobile.Focus();
+            }
+            else
+            {
+                errMobile.Visible = false;
+            }
+
+            if (radFemale.Checked == true)
+            {
+                gender = "F";
+                errGender.Visible = false;
+            }
+            else if (radMale.Checked == true)
+            {
+                gender = "M";
+                errGender.Visible = false;
+            }
+            else
+            {
+                errGender.Visible = true;
+            }
+
+            // end of text field and radio button validation
+
+            if (FirstName != "" && LastName != "" && Address != "" && Mobile != "" && Email != "" && gender != "")
+            {
+                string constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\rmaha\Desktop\assignment_1\assignment_1\assignment_1\App_Data\login.mdf;Integrated Security=True";
+                SqlConnection con = new SqlConnection(constr);
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO UserData ( FirstName, LastName, Email, Mobile, Address, Gender) VALUES( @FName, @LName, @Email, @Mobile, @Address, @Gender) ", con);
+                //  cmd.Parameters.AddWithValue("@Stu_ID", null);
+                cmd.Parameters.AddWithValue("@FName", FirstName);
+                cmd.Parameters.AddWithValue("@LName", LastName);
+                cmd.Parameters.AddWithValue("@Email", Email);
+                cmd.Parameters.AddWithValue("@Mobile", Mobile);
+                cmd.Parameters.AddWithValue("@Address", Address);
+                cmd.Parameters.AddWithValue("@Gender", gender);
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                con.Close();
+
+                btnReset_Click(null, null);
+
+                Response.Write("<script>alert('Saved Successfully);</script>");
+                Response.Write("<script LANGUAGE='JavaScript' >alert('Saved Successfully')</script>");
+            }
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtEmail.Text = "";
+            txtAddress.Text = "";
+            txtMobile.Text = "";
+            radFemale.Checked = false;
+            radMale.Checked = false;
+        }
+    }
+}
